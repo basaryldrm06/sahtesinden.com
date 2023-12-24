302c696f3196d5b0d7ff15bdf1fa4cc5bbdc3405
basaryldrm06 <mertbasaryildirim@etu.edu.tr>
2023-12-24 22:06:07 +0300

    bootstrap5 is added, navbar file is created

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ const express = require('express');
 const ip = require('ip');
 const path = require('path');
 
+const bootstrap5js = path.join(__dirname, 'node_modules', 'bootstrap', 'dist', 'css', 'bootstrap.min.css');
+const bootstrap5css = path.join(__dirname, 'node_modules', 'bootstrap', 'dist', 'js', 'bootstrap.bundle.min.js');
+
 const app = express();
 const PORT = process.env.PORT || 3000;
 const serverIP = ip.address();
@@ -10,8 +13,11 @@ app.set('view engine', 'pug');
 app.set('views', path.join(__dirname, "views"));
 
 app.get('/home', (request, response) => {
-    response.render("home");
+    response.render("pages/home");
 });
+
+app.use('bootstrap/css', express.static(bootstrap5css));
+app.use('bootstrap/js', express.static(bootstrap5js));
 
 app.listen(PORT, () => {
     console.log('\n\nSERVER HAS BEEN STARTED');
